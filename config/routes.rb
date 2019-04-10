87b12059251978d9731231d0ad204504d5e9f6a0
david medina <davidmeddina@gmail.com>
2019-04-10 13:04:15 -0500

    Products controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :products
+  root "products#index"
+  # get 'products/index'
+  # get 'products/new'
+  # get 'products/edit'
+  # get 'products/create'
+  # get 'products/update'
+  # get 'products/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
